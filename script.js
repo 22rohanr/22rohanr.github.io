70947eb0e5afe0805b2a4d2b27e0d3bdb161015c
Rohan Rashingkar <45675224+22rohanr@users.noreply.github.com>
2023-06-20 22:48:14 -0500

    first draft

---
+++ script.js
@@ -0,0 +1,34 @@
+// Get references to the buttons
+const homeButton = document.getElementById('home-button');
+const projectsButton = document.getElementById('projects-button');
+const linkedinButton = document.getElementById('linkedin-button');
+const githubButton = document.getElementById('github-button');
+const nameButton = document.getElementById('name-button');
+
+// Add event listeners to the buttons
+homeButton.addEventListener('click', handleHomeClick);
+projectsButton.addEventListener('click', handleProjectsClick);
+linkedinButton.addEventListener('click', handleLinkedinClick);
+githubButton.addEventListener('click', handleGithubClick);
+nameButton.addEventListener('click', handleNameClick)
+
+
+function handleHomeClick() {
+    window.location.href = 'index.html';
+}
+
+function handleProjectsClick() {
+    window.location.href = 'projects.html';
+}
+
+function handleLinkedinClick() {
+    window.location.href = 'https://www.linkedin.com/in/rohan-rashingkar/';
+}
+
+function handleGithubClick() {
+    window.location.href = 'https://github.com/22rohanr/';
+}
+
+function handleNameClick() {
+    window.location.href = 'index.html';
+}
